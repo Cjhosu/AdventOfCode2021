d42999e40dec73dfcbd1b580f082a5c00b9b5d5a
Caleb Howard <choward@covermymeds.com>
2022-11-14 10:36:08 -0500

    card creation function

--- Four/day_four_solution.py
+++ Four/day_four_solution.py
@@ -12,14 +12,36 @@ with open('day_four_sample') as file:
 
 bingo = list(filter(lambda a: a != '', bingo))
 
-print(call_nums)
-print(bingo)
 
-counter =1
+card_num =1
+card_line_num = 1
+
+col1 = []
+col2 = []
+col3 = []
+col4 = []
+col5 = []
+
+row1 = []
+row2 = []
+row3 = []
+row4 = []
+row5 = []
+
+
+def create_card(card_num, full_line, card_line_num):
+    print(full_line, card_num, card_line_num)
 
 for i in bingo:
     l = list(i.split(' '))
     l= list(filter(lambda a: a != '', l))
     print(l)
-
+    if card_line_num <= 5:
+        create_card(card_num, l, card_line_num)
+        card_line_num += 1
+    else:
+        card_line_num = 1
+        card_num += 1
+        create_card(card_num, l, card_line_num)
+        card_line_num += 1
 
